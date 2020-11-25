841d8ad6e2c66c7d9c2680fc2d530c307478d032
Josiah Arkson <34874253+josiaharkson@users.noreply.github.com>
2020-11-25 13:04:36 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { connect } from "react-redux";
-
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IsLoading from "./components/IsLoading";
 
@@ -20,6 +20,21 @@ console.log(process.env);
 function App(props) {
   const [loadingUser, setLoadingUser] = React.useState(true);
 
+   const wakeAPI = async () => {
+    try {
+      const res = await axios.get(
+        "https://agro-mart10.herokuapp.com/api/farm/get/5f69aa672923a47340ccdd98"
+      );
+
+      console.log(res.data);
+    } catch (e) {}
+  };
+  wakeAPI();
+
+  React.useEffect(() => {
+    wakeAPI();
+  });
+  
   React.useEffect(() => {
     const user = getCurrentUser();
     if (user) {
